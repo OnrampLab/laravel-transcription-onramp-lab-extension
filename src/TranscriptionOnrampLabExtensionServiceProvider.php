ae14271d25ede783984b103e7c228b339d28e128
Eric Chang <ericsonc510@gmail.com>
2023-08-29 18:08:30 +0800

    chore(config): update class namespace

---
+++ src/TranscriptionOnrampLabExtensionServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OnrampLab\TranscriptionOnrampLabExtension;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranscriptionOnrampLabExtensionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
